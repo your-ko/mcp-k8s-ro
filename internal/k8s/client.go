171a33a6c926b11f3759e0049958634f7920f44c
yourik <mcp-k8s-ro@your-ko.me>
2026-03-17 08:06:40 +0100

    add node condition/status

--- internal/k8s/client.go
+++ internal/k8s/client.go
@@ -181,6 +181,24 @@ func setResourceSpecificFields(item unstructured.Unstructured, res *listResource
 				}
 			}
 		}
+		if conditions, ok, err := unstructured.NestedSlice(item.Object, "status", "conditions"); ok && err == nil {
+			conditionsStatus := make([]string, 0, len(conditions))
+			for _, condition := range conditions {
+				conditionMap := condition.(map[string]string)
+				if status, ok := conditionMap["status"]; ok && status == "True" {
+					if typ, ok := conditionMap["Type"]; ok {
+						if typ == "Ready" {
+							// between all node conditions we found Status:Ready, so there is no need to continue
+							res.Status = typ
+							break
+						} else {
+							conditionsStatus = append(conditionsStatus, typ)
+						}
+					}
+				}
+			}
+			res.Status = strings.Join(conditionsStatus, ",")
+		}
 	}
 }
 
