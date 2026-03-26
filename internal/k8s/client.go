2cdd9bca6bc5a89514e1284592ccb54c90464ca4
yourik <mcp-k8s-ro@your-ko.me>
2026-03-26 08:55:06 +0100

    slightly optimise memory use, skip unnecessary deep-copies

--- internal/k8s/client.go
+++ internal/k8s/client.go
@@ -504,10 +504,12 @@ func sanitize(item *unstructured.Unstructured) error {
 			}
 		}
 	case "CertificateSigningRequest":
-		if spec, ok, err := unstructured.NestedMap(item.Object, "spec"); ok && err == nil {
-			if _, ok := spec["request"]; ok {
-				if err := unstructured.SetNestedField(item.Object, "*****", "spec", "request"); err != nil {
-					return err
+		if spec, ok, err := unstructured.NestedFieldNoCopy(item.Object, "spec"); ok && err == nil {
+			if specMap, ok := spec.(map[string]interface{}); ok {
+				if _, ok := specMap["request"]; ok {
+					if err := unstructured.SetNestedField(item.Object, "*****", "spec", "request"); err != nil {
+						return err
+					}
 				}
 			}
 		}
